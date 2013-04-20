3d9f21b1679778b43311ccb9e3d98fbad0e17f5d
pc <pc@pc-PC>
2013-04-20 02:51:06 +0200

    commit gitbeans1

---
+++ src/testgitbeans/Testgitbeans.java
@@ -0,0 +1,11 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testgitbeans;
+public class Testgitbeans {
+    public static void main(String[] args)
+    {
+        
+    }
+}
